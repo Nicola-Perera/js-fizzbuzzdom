5f541abb9d2d0a58a5629a3698719a3cd5404f29
Nicola-Perera <nicola.perera96@mail.com>
2021-10-13 19:18:17 +0200

    fixed background colors

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,26 +5,27 @@ const numbersTable = document.getElementById('table');
 // ciclo per aggiungere numeri 1-100 ad html
 for (let i = 1; i < 101; i++) {
 
+    // multipli di 15
     if (i % 3 == 0 && i % 5 == 0) {
-        const cell = `<li class" number_${i} multiple_15">${'fizzbuzz'}</li>`;
+        const cell = `<li class=" number_${i} multiple_15">${'fizzbuzz'}</li>`;
 
         numbersTable.innerHTML += cell;
     } 
-    
+    // multipli di 3
     else if (i % 3 == 0) {
-        const cell = `<li class"number_${i} multiple_3">${'fizz'}</li>`;
+        const cell = `<li class="number_${i} multiple_3">${'fizz'}</li>`;
 
         numbersTable.innerHTML += cell;
     }
-
+    // multipli di 5
     else if (i % 5 == 0){
-        const cell = `<li  class"number_${i} multiple_5">${'buzz'}</li>`;
+        const cell = `<li  class="number_${i} multiple_5">${'buzz'}</li>`;
 
         numbersTable.innerHTML += cell;
     }
-
+    // numeri indivisibili per 3 e per 5
     else {
-        const cell = `<li class"number_${i}">${i}</li>`;
+        const cell = `<li class="number_${i}">${i}</li>`;
 
     numbersTable.innerHTML += cell;
     }
