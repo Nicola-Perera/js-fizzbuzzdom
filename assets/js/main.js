83c13d19181afb03947325eb16b43ad986532f80
Nicola-Perera <nicola.perera96@mail.com>
2021-10-13 18:34:04 +0200

    added colors to multiples

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,24 +7,30 @@ console.log(numbersTable);
 for (let i = 1; i < 101; i++) {
 
     if (i % 3 == 0 && i % 5 == 0) {
-        const cell = `<li class"number_${i}">${'fizzbuzz'}</li>`;
+        const cell = `<li id="multiple_15" class"number_${i} multiple_15">${'fizzbuzz'}</li>`;
         console.log(cell);
 
         numbersTable.innerHTML += cell;
+        
+        document.querySelector('#multiple_15').style.backgroundColor = '#DE5471';
     } 
     
     else if (i % 3 == 0) {
-        const cell = `<li class"number_${i}">${'fizz'}</li>`;
+        const cell = `<li id="multiple_3" class"number_${i}">${'fizz'}</li>`;
         console.log(cell);
 
         numbersTable.innerHTML += cell;
+
+        document.querySelector('#multiple_3').style.backgroundColor = '#61D2A4';
     }
 
     else if (i % 5 == 0){
-        const cell = `<li class"number_${i}">${'buzz'}</li>`;
+        const cell = `<li id="multiple_5" class"number_${i}">${'buzz'}</li>`;
         console.log(cell);
 
         numbersTable.innerHTML += cell;
+
+        document.querySelector('#multiple_5').style.backgroundColor = '#F9D277';
     }
 
     else {
