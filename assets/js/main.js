917b7a748bf37267d3fb323755ccf77ebb75eb72
Nicola-Perera <nicola.perera96@mail.com>
2021-10-13 17:31:37 +0200

    add if/else to distinguish multiples of 3 and 5

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,32 @@ console.log(numbersTable);
 
 // ciclo per aggiungere numeri 1-100 ad html
 for (let i = 1; i < 101; i++) {
-    const cell = `<li class"number_${i}">${i}</li>`;
+
+    if (i % 3 == 0 && i % 5 == 0) {
+        const cell = `<li class"number_${i}">${'fizzbuzz'}</li>`;
+        console.log(cell);
+
+        numbersTable.innerHTML += cell;
+    } 
+    
+    else if (i % 3 == 0) {
+        const cell = `<li class"number_${i}">${'fizz'}</li>`;
+        console.log(cell);
+
+        numbersTable.innerHTML += cell;
+    }
+
+    else if (i % 5 == 0){
+        const cell = `<li class"number_${i}">${'buzz'}</li>`;
+        console.log(cell);
+
+        numbersTable.innerHTML += cell;
+    }
+
+    else {
+        const cell = `<li class"number_${i}">${i}</li>`;
     console.log(cell);
 
     numbersTable.innerHTML += cell;
+    }
 }
